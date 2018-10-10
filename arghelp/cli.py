fbb95ed12f15f53ea762e7bc3ade1c641f547e25
Michael V. DePalatis <mike@depalatis.net>
2018-10-10 11:30:01 -0600

    Refactor as a class

--- arghelp/cli.py
+++ arghelp/cli.py
@@ -1,7 +1,5 @@
-from argparse import ArgumentParser
-
-cli = ArgumentParser()
-subparsers = cli.add_subparsers(dest="subcommand")
+from argparse import ArgumentParser, Namespace
+from typing import List, Optional
 
 
 def argument(*name_or_flags, **kwargs):
@@ -11,57 +9,58 @@ def argument(*name_or_flags, **kwargs):
     """
     return (list(name_or_flags), kwargs)
 
-
-def subcommand(args=[], parent=subparsers):
-    """Decorator to define a new subcommand in a sanity-preserving way.
-    The function will be stored in the ``func`` variable when the parser
-    parses arguments so that it can be called directly like so::
-
-        args = cli.parse_args()
-        args.func(args)
-
-    Usage example::
-
-        @subcommand([argument("-d", help="Enable debug mode", action="store_true")])
-        def subcommand(args):
-            print(args)
-
-    Then on the command line::
-
-        $ python cli.py subcommand -d
-
-    """
-    def decorator(func):
-        parser = parent.add_parser(func.__name__, description=func.__doc__)
-        for arg in args:
-            parser.add_argument(*arg[0], **arg[1])
-        parser.set_defaults(func=func)
-    return decorator
+# shortcut
+arg = argument
 
 
-@subcommand()
-def nothing(args):
-    print("Nothing special!")
+class Application(object):
+    """A command-line application builder."""
+    def __init__(self):
+        self.cli = ArgumentParser()
+        self.subparsers = self.cli.add_subparsers(dest="subcommand")
 
+    def subcommand(self, args=[], parent=None):
+        """Decorator to define a new subcommand in a sanity-preserving way.
+        The function will be stored in the ``func`` variable when the parser
+        parses arguments so that it can be called directly like so::
 
-@subcommand([argument("-d", help="Debug mode", action="store_true")])
-def test(args):
-    print(args)
+            args = cli.parse_args()
+            args.func(args)
 
+        Usage example::
 
-@subcommand([argument("-f", "--filename", help="A thing with a filename")])
-def filename(args):
-    print(args.filename)
+            cli = Application()
 
+            @cli.subcommand([arg("-d", help="Enable debug mode", action="store_true")])
+            def subcommand(args):
+                print(args)
 
-@subcommand([argument("name", help="Name")])
-def name(args):
-    print(args.name)
+        Then on the command line::
 
+            $ python cli.py subcommand -d
 
-if __name__ == "__main__":
-    args = cli.parse_args()
-    if args.subcommand is None:
-        cli.print_help()
-    else:
-        args.func(args)
+        """
+        parent = parent or self.subparsers
+
+        def decorator(func):
+            parser = parent.add_parser(func.__name__, description=func.__doc__)
+            for arg in args:
+                parser.add_argument(*arg[0], **arg[1])
+            parser.set_defaults(func=func)
+        return decorator
+
+    def parse_args(self, args: Optional[List[str]] = None,
+                   namespace: Optional[Namespace] = None) -> Namespace:
+        """Parse and return command line arguments."""
+        return self.cli.parse_args(args, namespace)
+
+    def main(self) -> None:
+        """Parse command line arguments and run a subcommand."""
+        args = self.parse_args()
+
+        if args.subcommand is None:
+            self.cli.print_help()
+            return
+
+        else:
+            args.func(args)
