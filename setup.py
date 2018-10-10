4e9082e7cd0c4984d7f42a446f080eaacc2ea630
Michael V. DePalatis <mike@depalatis.net>
2018-10-10 16:48:27 -0600

    Update setuptools and testing stuff

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from arghelp import __version__
+
+with open("README.rst", "r") as readme:
+    long_description = readme.read()
 
 setup(
     name="arghelp",
     version=__version__,
-    packages=["arghelp"],
+    packages=find_packages(include="arghelp"),
     author="Michael V. DePalatis",
     author_email="mike@depalatis.net",
-    url="",
+    url="https://github.com/mivade/arghelp",
     license="TBD",
     description="argparse helpers",
-    long_description="argparse helpers",
-    classifiers=[],
+    long_description=long_description,
+    setup_requires=[
+        "pytest-runner",
+    ],
+    tests_require=[
+        "pytest",
+        "pytest-cov",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: User Interfaces",
+    ],
 )
