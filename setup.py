c946c1afdc2f77c6023accbb3ba813d0757c85c3
Michael V. DePalatis <mike@depalatis.net>
2018-10-10 11:13:15 -0600

    Make into a package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from arghelp import __version__
+
+setup(
+    name="arghelp",
+    version=__version__,
+    packages=["arghelp"],
+    author="Michael V. DePalatis",
+    author_email="mike@depalatis.net",
+    url="",
+    license="TBD",
+    description="argparse helpers",
+    long_description="argparse helpers",
+    classifiers=[],
+)
